5973a51b63718e4198b83d3c9ffa72770162ab25
Peter <petiodjonov@yahoo.com>
2024-01-20 17:56:27 +0200

    Add better routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+// App.tsx
+import React, { useState } from 'react';
+import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { AuthContext, AuthState } from './authService';
 import LogIn from './components/LogIn';
 import UserList from './components/UserList';
-import './styles/login.css';
 
-const App: React.FC = () => {
+function App() {
+  const [auth, setAuth] = useState<AuthState>({ isAuthenticated: false, user: null });
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/user-list" element={<UserList />} />
-        <Route path="/" element={<LogIn />} />
-      </Routes>
-    </Router>
+    <AuthContext.Provider value={{ auth, setAuth }}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/login" />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/users" element={<UserList />} />
+          {/* ... other routes ... */}
+        </Routes>
+      </Router>
+    </AuthContext.Provider>
   );
-};
+}
 
 export default App;
