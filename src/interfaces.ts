7034de364d42953fa14fe944b05fab47df9ecbe9
Peter <petiodjonov@yahoo.com>
2024-01-20 18:19:54 +0200

    Add additional reusable interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,15 +1,31 @@
+import { Dispatch, SetStateAction } from 'react';
 
 export interface User {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    gender: string;
-    ip_address: string;
-    friends: Friend[];
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  ip_address?: string;
+  gender?: string;
+  friends?: Friend[];
 }
   
 export interface Friend {
     id: number;
     name: string;
+}
+
+interface AuthUser {
+  username: string;
+  role: string;
+}
+
+export interface AuthState {
+  isAuthenticated: boolean;
+  user: AuthUser | null;
+}
+
+export interface AuthContextType {
+  auth: AuthState;
+  setAuth: Dispatch<SetStateAction<AuthState>>;
 }
