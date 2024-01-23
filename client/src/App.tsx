87cfbbe29fad7e9a3273e609cc9324fa971347f8
Peter <petiodjonov@yahoo.com>
2024-01-23 17:46:39 +0200

    Fix input field autofilling data on LoginRegister

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,32 +8,53 @@ import { User } from './interfaces';
 import './styles/App.css';
 import './styles/login.css';
 
+const initialLoginUserData = {
+    email: '',
+    password: '',
+};
+
+const initialRegisterUserData = {
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    gender: '',
+    role: '',
+};
+
+const clearUserData = (isLogin: boolean, setUserData: any ) => {
+    if (isLogin) {
+        setUserData({ ...initialLoginUserData });
+    } else {
+        setUserData({ ...initialRegisterUserData });
+    }
+};
+
 const App: React.FC = () => {
     const socket: Socket = io.connect('http://localhost:3001');
-    const [userData, setUserData] = useState<any>({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        gender: '',
-        role: '',
-    });
+
+    // Separate userData states for Register and Login forms
+    const [loginUserData, setLoginUserData] = useState<any>(initialLoginUserData)
+    const [registerUserData, setRegisterUserData] = useState<any>(initialRegisterUserData)
+
     const [loginMessage] = useState('');
     const [showTable, setShowTable] = useState(false);
     const [isLogin, setIsLogin] = useState(true);
 
-    const ShowLogin =(value: boolean)=> {
-      setIsLogin(value);
-    }
+    const ShowLogin = (value: boolean) => {
+        setIsLogin(value);
+        clearUserData(value, isLogin ? setLoginUserData : setRegisterUserData);
+    };
 
     const login = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault(); // Prevent page reload
-        if (userData.email && userData.password) {
-            socket.emit('log_in', { email: userData.email, password: userData.password });
-            
+        if (loginUserData.email && loginUserData.password) {
+            socket.emit('log_in', { email: loginUserData.email, password: loginUserData.password });
+
             socket.on('login_response', (message, user) => {
                 if (message === 'Success') {
-                    setUserData(user); // Update user data in the state
+                    // Update user data in the state
+                    setLoginUserData(user);
                     setShowTable(true); // Show the table after successful login
                     localStorage.setItem('isLoggedIn', 'true');
                 } else {
@@ -51,25 +72,24 @@ const App: React.FC = () => {
         let fieldsFilled = true;
 
         // Check if all required fields are filled
-        for (const key in userData) {
-            if (!notEmpty(userData[key])) {
+        for (const key in registerUserData) {
+            if (!notEmpty(registerUserData[key])) {
                 fieldsFilled = false;
                 break; // Break the loop if any field is empty
             }
         }
 
         if (fieldsFilled) {
-            socket.emit('register', userData);
+            socket.emit('register', registerUserData);
 
             socket.on('registration_response', (message, newUser) => {
                 if (message === 'User exists') {
                     alert('User with the same information already exists.');
-                } 
-                else if (message === 'Email exists'){
+                } else if (message === 'Email exists') {
                     alert('Email already used.');
-                }
-                else {
-                    setUserData(newUser); // Update user data in the state
+                } else {
+                    // Update user data in the state
+                    setLoginUserData(newUser);
                     setShowTable(true); // Set showTable to true upon successful register
                     localStorage.setItem('isLoggedIn', 'true');
                 }
@@ -82,11 +102,11 @@ const App: React.FC = () => {
     const logout = () => {
         // Reset userData state
         const resetData: Partial<User> = {};
-        Object.keys(userData).forEach(key => {
+        Object.keys(loginUserData).forEach(key => {
             resetData[key as keyof User] = ''; // Set each field to an empty string
         });
-        setUserData(resetData as User);
-        
+        setLoginUserData(resetData as User);
+
         // Hide the table and remove user data from local storage
         setShowTable(false);
         localStorage.removeItem('userData');
@@ -97,18 +117,18 @@ const App: React.FC = () => {
         const isLoggedIn = localStorage.getItem('isLoggedIn');
         if (savedUserData && isLoggedIn === 'true') {
             const parsedUserData = JSON.parse(savedUserData);
-            setUserData(parsedUserData);
+            setLoginUserData(parsedUserData);
             setShowTable(true); // Set showTable to true if the user is already logged in
         }
-    },[]);
+    }, []);
 
     return (
         <div>
             {!showTable ? (
                 <LogIn
                     isLogin={isLogin}
-                    userData={userData}
-                    setUserData={setUserData}
+                    userData={isLogin ? loginUserData : registerUserData}
+                    setUserData={isLogin ? setLoginUserData : setRegisterUserData}
                     login={login}
                     register={register}
                     ShowLogin={ShowLogin}
@@ -117,20 +137,22 @@ const App: React.FC = () => {
                 <Container className="mt-4 chat-window">
                     <Card>
                         <Card.Header className='chat-header d-flex justify-content-between align-items-center'>
-                            <h5>Welcome {loginMessage || ('back ' + userData.first_name + ' ' + userData.last_name + '!')} ({userData.role})</h5>
+                            <h5>
+                                Welcome {loginMessage || ('back ' + loginUserData.first_name + ' ' + loginUserData.last_name + '!')} ({loginUserData.role})
+                            </h5>
                             <Button className='logout-button' variant="success" onClick={logout}><b>Log out</b></Button>
                         </Card.Header>
                     </Card>
-                    <UserList userData={userData}/>
-                    <div>Email: {userData.email}</div>
-                    <div>Password: {userData.password}</div>
-                    <div>Gender: {userData.gender}</div>
+                    <UserList userData={loginUserData} />
+                    <div>Email: {loginUserData.email}</div>
+                    <div>Password: {loginUserData.password}</div>
+                    <div>Gender: {loginUserData.gender}</div>
                     <div></div>
                     <div></div>
                 </Container>
             )}
         </div>
     );
-}
+};
 
 export default App;
--- client/src/components/LogIn.tsx
+++ client/src/components/LogIn.tsx
@@ -1,22 +1,21 @@
 import React, { FormEvent } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 
-interface LoginRegisterProps {
+interface LogInProps {
     isLogin: boolean;
-    ShowLogin: (value: boolean) => void;
     userData: any;
     setUserData: (userData: any) => void;
     login: (event: FormEvent<HTMLFormElement>) => void;
     register: (event: FormEvent<HTMLFormElement>) => void;
+    ShowLogin: (value: boolean) => void;
 }
 
-const LoginRegister: React.FC<LoginRegisterProps> = ({ isLogin, ShowLogin, userData, setUserData, login, register }) => {
-
+const LogIn: React.FC<LogInProps> = ({ isLogin, userData, setUserData, login, register, ShowLogin }) => {
     const handleChange = (e: any) => {
         const { name, value } = e.target;
         setUserData({
             ...userData,
-            [name]: value
+            [name]: value,
         });
     };
 
@@ -26,31 +25,68 @@ const LoginRegister: React.FC<LoginRegisterProps> = ({ isLogin, ShowLogin, userD
                 <Container fluid>
                     <Row>
                         <Col md={6} className="loginBoxStyle">
+                            <Form onSubmit={login}>
+                                <h2 className='d-flex justify-content-center'>Login</h2>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        name="email"
+                                        value={userData.email}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        name="password"
+                                        value={userData.password}
+                                        onChange={handleChange}
+                                    />
+                                </Form.Group>
+                                <Button variant="primary" type="submit" className='mt-3'>
+                                    Log In
+                                </Button>
+                                <Button variant="link" type="button" className='mt-3' onClick={() => ShowLogin(false)}>
+                                    <b>Register</b>
+                                </Button>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Container>
+            ) : (
+                <Container fluid>
+                    <Row>
+                        <Col md={6} className="loginBoxStyle">
                             <Form onSubmit={register}>
                                 <h2 className='d-flex justify-content-center'>Register</h2>
                                 <Form.Group className="mb-3">
-                                  <Form.Label>First Name</Form.Label>
-                                  <Form.Control
-                                      type="text"
-                                      name="first_name"
-                                      value={userData.first_name}
-                                      onChange={handleChange}/>
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="first_name"
+                                        value={userData.first_name}
+                                        onChange={handleChange}
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-3">
-                                  <Form.Label>Last Name</Form.Label>
-                                  <Form.Control
-                                    type="text"
-                                    name="last_name"
-                                    value={userData.last_name}
-                                    onChange={handleChange}/>
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        name="last_name"
+                                        value={userData.last_name}
+                                        onChange={handleChange}
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-3">
-                                  <Form.Label>Email</Form.Label>
-                                  <Form.Control
-                                    type="email"
-                                    name="email"
-                                    value={userData.email}
-                                    onChange={handleChange}/>
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control
+                                        type="email"
+                                        name="email"
+                                        value={userData.email}
+                                        onChange={handleChange}
+                                    />
                                 </Form.Group>
                                 <Form.Group className="mb-3">
                                     <Form.Label>Password</Form.Label>
@@ -62,81 +98,50 @@ const LoginRegister: React.FC<LoginRegisterProps> = ({ isLogin, ShowLogin, userD
                                     />
                                 </Form.Group>
                                 <Form.Group>
-                                  <Form.Label><b>Gender</b> </Form.Label>
-                                  <div key='gender' className="mb-3">
-                                    {["Male", "Female", "Other", "Don't Specify"].map((gender) => (
-                                      <Form.Check 
-                                      inline 
-                                      key={gender}
-                                      label={gender} 
-                                      type="radio" 
-                                      id={`gender-${gender}`}
-                                      value={gender}
-                                      checked={userData.gender === gender}
-                                      onChange={() => setUserData({ ...userData, gender })}/>
-                                    ))}
-                                  </div>
+                                    <Form.Label><b>Gender</b> </Form.Label>
+                                    <div key='gender' className="mb-3">
+                                        {["Male", "Female", "Other", "Don't Specify"].map((gender) => (
+                                            <Form.Check
+                                                inline
+                                                key={gender}
+                                                label={gender}
+                                                type="radio"
+                                                id={`gender-${gender}`}
+                                                value={gender}
+                                                checked={userData.gender === gender}
+                                                onChange={() => setUserData({ ...userData, gender })}
+                                            />
+                                        ))}
+                                    </div>
                                 </Form.Group>
                                 <Form.Group>
-                                  <Form.Label><b>Role</b></Form.Label>
-                                  <div key='role' className="mb-3">
-                                    {["admin", "super-user", "user"].map((role) => (
-                                      <Form.Check 
-                                        inline 
-                                        key={role} 
-                                        label={role} 
-                                        type="radio" 
-                                        id={`role-${role}`}
-                                        value={role}
-                                        checked={userData.role === role} // Check if userData.role matches the current role
-                                        onChange={() => setUserData({ ...userData, role })} // Update userData.role on change
-                                      />
-                                    ))}
-                                  </div>
+                                    <Form.Label><b>Role</b></Form.Label>
+                                    <div key='role' className="mb-3">
+                                        {["admin", "super-user", "user"].map((role) => (
+                                            <Form.Check
+                                                inline
+                                                key={role}
+                                                label={role}
+                                                type="radio"
+                                                id={`role-${role}`}
+                                                value={role}
+                                                checked={userData.role === role}
+                                                onChange={() => setUserData({ ...userData, role })}
+                                            />
+                                        ))}
+                                    </div>
                                 </Form.Group>
                                 <Button variant="primary" type="submit" className='mt-3'> Register </Button>
-                                <Button variant="link" type="submit" className='mt-3' onClick={() => ShowLogin(false)}>
-                                  <b>Login</b>
+                                <Button variant="link" type="button" className='mt-3' onClick={() => ShowLogin(true)}>
+                                    <b>Login</b>
                                 </Button>
                             </Form>
                         </Col>
                     </Row>
                 </Container>
-            ) : (
-              <Container fluid>
-                <Row>
-                  <Col md={6} className="loginBoxStyle">
-                    <Form onSubmit={login}>
-                      <h2 className='d-flex justify-content-center'>Login</h2>
-                      <Form.Group className="mb-3">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                          type="email"
-                          name="email"
-                          value={userData.email}
-                          onChange={handleChange}/>
-                      </Form.Group>
-                      <Form.Group className="mb-3">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="password"
-                          name="password"
-                          value={userData.password}
-                          onChange={handleChange}/>
-                      </Form.Group>
-                      <Button variant="primary" type="submit" className='mt-3'>
-                        Log In
-                      </Button>
-                      <Button variant="link" type="submit" className='mt-3' onClick={() => ShowLogin(true)}>
-                        <b>Register</b>
-                      </Button>
-                    </Form>
-                  </Col>
-                </Row>
-              </Container>
             )}
         </div>
     );
 };
 
-export default LoginRegister;
+export default LogIn;
